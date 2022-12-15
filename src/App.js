55569089a11326c9eb02b01b00aaa64122568bd5
seymagundogduu <symagn2@gmail.com>
2022-12-15 17:24:55 +0300

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Content from './components/Content';
+import ContentFooter from './components/ContentFooter';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='todoapp'>
+      <Header/>
+      <Content />
+   
+    </section>
+    <Footer />
+    </>
+  
   );
 }
 
---
+++ src/components/Header/NewToDoForm/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Formik, Field, Form, useFormik } from "formik";
+import validationSchema from "./validations";
+function NewTodoForm() {
+ 
+  return (
+    <Formik
+      initialValues={{
+        text: " "
+      }}
+      onSubmit={ (values, bag) => {
+      
+        console.log(values);
+
+        bag.resetForm();
+      }}
+
+      validationSchema={validationSchema}
+    >
+
+   
+    <Form>
+      <Field
+        className="new-todo"
+        placeholder="What needs to be done?"
+        autoFocus
+        id="text"
+        name="text"
+      />
+    </Form>
+    </Formik>
+  );
+}
+
+export default NewTodoForm;
