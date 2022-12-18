cfac2cc6f938812e508cc90f39b67e124efd4ddc
seymagundogduu <symagn2@gmail.com>
2022-12-18 15:31:57 +0300

    delete todo

--- src/components/Content/List/Item.js
+++ src/components/Content/List/Item.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { useTodo } from '../../../contexts/TodoContext';
 function Item({todo}) {
-  const {toggleTodo} = useTodo();
+  const { toggleTodo, destroyTodo} = useTodo();
 
-  const onChange = (id) => {
-    toggleTodo(id)
-  };
+  const onChange = (id) => toggleTodo(id);
+
+  const onDestroy = (id) =>  destroyTodo(id);
+
   return (
     <div> 
      <li key={todo.id} className={todo.completed ? "completed" : ""}>
     <div className="view">
       <input className="toggle" type="checkbox" checked={todo.completed} onChange={() => onChange(todo.id)}/>
       <label>{todo.text}</label>
-      <button className="destroy"></button>
+      <button className="destroy" onClick={() => onDestroy(todo.id)}></button>
     </div>
   </li></div>
   )
