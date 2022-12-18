e467f820bfd4853ac14d2fade9eb16103ac0f562
seymagundogduu <symagn2@gmail.com>
2022-12-18 15:12:31 +0300

    todo checkbox

--- src/components/Content/List/Item.js
+++ src/components/Content/List/Item.js
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import { useTodo } from '../../../contexts/TodoContext';
 function Item({todo}) {
+  const {toggleTodo} = useTodo();
+
+  const onChange = (id) => {
+    toggleTodo(id)
+  };
   return (
     <div> 
      <li key={todo.id} className={todo.completed ? "completed" : ""}>
     <div className="view">
-      <input className="toggle" type="checkbox" checked={todo.completed}/>
+      <input className="toggle" type="checkbox" checked={todo.completed} onChange={() => onChange(todo.id)}/>
       <label>{todo.text}</label>
       <button className="destroy"></button>
     </div>
